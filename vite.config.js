8e80e26769e630d3d24c2bb3b369dece2c8a3c1f
dilip <dilip@gmail.com>
2025-10-29 20:50:46 +0530

    m

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'qgmn5c-5173.csb.app', // 👈 Add your host here
+    ],
+  },
 })
